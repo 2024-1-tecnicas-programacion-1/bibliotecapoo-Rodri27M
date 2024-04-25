efe09ad9660df79c33b9b6ed03037933fad63503
A <santiago2003victor@gmail.com>
2024-04-25 10:16:49 -0500

    Taller libro biblioteca

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,15 @@
 package com.mycompany.bibliotecapoo;
 
+import java.util.LinkedList;
+
 public class Biblioteca {
     // TODO: Aquí va tu código
+    private LinkedList <Libro> libros;
+    public Biblioteca(){
+        this.libros = new LinkedList<>();
+    }
+    
+    public void registrarLibro(LinkedList libros){
+        libros.add();
+    }
 }
--- src/main/java/com/mycompany/bibliotecapoo/Principal.java
+++ src/main/java/com/mycompany/bibliotecapoo/Principal.java
@@ -1,6 +1,12 @@
 package com.mycompany.bibliotecapoo;
 
+import java.util.Scanner;
+
 
 public class Principal {
     // TODO: Aquí va tu código
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
 }
